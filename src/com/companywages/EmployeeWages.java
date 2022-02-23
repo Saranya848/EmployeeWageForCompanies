1bfa2834ea9c0d533914344de1178d72aeab6d67
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-02-23 23:26:25 +0530

    Calculating Monthly Wages

--- src/com/companywages/EmployeeWages.java
+++ src/com/companywages/EmployeeWages.java
@@ -2,41 +2,46 @@ package com.companywages;
 
 public class EmployeeWages {
 	public static int getRandomNumber() {
-		//Generate random number for 0 for absent 1 for present
-		int empCheck=(int)Math.floor(Math.random()*10) % 3;
-		//System.out.println(empCheck);
+		// Generate random number for 0 for absent 1 for present
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		// System.out.println(empCheck);
 		return empCheck;
 
 	}
-	public static void empdata(int empCheck) {
-		final int wagesperhrs = 20;
-		int empHrs = 0;
-		// number for 0 for absent 1 for present
-		empCheck=getRandomNumber();
 
-		// Switch to check employee is working full time, part time or absent
-		switch (empCheck) {
-		case 0:
-			empHrs = 0;// Employee absent
-			break;
-		case 1:
-			empHrs = 4;// Employee Parttime present
-			break;
-		default:
-			empHrs = 8;// Employee Fulltime present
-			break;
+	public static void empinfo(int empCheck) {
+		final int wagesperhrs = 120, workingday = 20;
+		int empHrs = 0, dailywages = 0;
+		int monthlyWage = 0;
+		// number for 0 for absent 1 for present
+		empCheck = getRandomNumber();
+		// looping for n working days
+		for (int day = 1; day <= workingday; day++) {
+			// Switch to check employee is working full time, part time or absent
+			switch (empCheck) {
+			case 0:
+				empHrs = 0;// Employee absent
+				break;
+			case 1:
+				empHrs = 4;// Employee Parttime present
+				break;
+			default:
+				empHrs = 8;// Employee Fulltime present
+				break;
+			}
+			// Calculating Daily Wages and Monthly Wages of employee
+			dailywages = wagesperhrs * empHrs;
+			monthlyWage = monthlyWage + dailywages;
 		}
-		// Calculating Daily Wages of employee
-		int dailywages = wagesperhrs * empHrs;
-		System.out.println("Employee Wage is " + dailywages);
+		System.out.println("Employee Wage daily is " + dailywages + " and Monthly Wage is " + monthlyWage);
 
 	}
 
 	public static void main(String[] args) {
-		int empCheck=0;
+		int empCheck = 0;
 		getRandomNumber();
 		// declaring constants
-		empdata(empCheck);
+		empinfo(empCheck);
 	}
 
 }
