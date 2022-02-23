2aed51fd269ebc6d78981d533965548edd6a3a4f
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-02-24 00:04:19 +0530

    Calculating diff company wages

--- src/com/companywages/EmployeeWages.java
+++ src/com/companywages/EmployeeWages.java
@@ -1,27 +1,19 @@
 package com.companywages;
 
 class Wages {
-	public static int getRandomNumber() {
-		// Generate random number for 0 for absent 1 for present
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		// System.out.println(empCheck);
-		return empCheck;
-	}
 
-	public static void empdetails(int empCheck) {
-		final int wagesperhrs = 120, workingday = 20;
+	public void empdetails(String CompanyName, int wagesperhrs, int workinghours, int workingday) {
 		int dailywages = 0;
 		int monthlyWage = 0;
 		// looping for n working days
 		int totalempHrs = 0, totalWage = 0;
 		int totalworkingdays = 0;
-		int workinghours = 100;
 
 		while (totalempHrs <= workinghours && totalworkingdays <= workingday) {
 			int empHrs = 0;
 			totalworkingdays++;
-			// number for 0 for absent 1 for present
-			empCheck = getRandomNumber();
+			// Generate random number for 0 for absent 1 for present
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			// Switch to check employee is working full time, part time or absent
 			switch (empCheck) {
 			case 0:
@@ -40,12 +32,12 @@ class Wages {
 			monthlyWage = monthlyWage + dailywages;
 			// Total Hour calculation
 			totalempHrs = totalempHrs + empHrs;
-			System.out.println(totalempHrs);
+			System.out.println("Working Hours: " + totalworkingdays);
+			System.out.println("Employee Hours : " + totalempHrs);
 			// calculating Total Salary
 			totalWage = totalempHrs * wagesperhrs;
 		}
-		System.out.println("Employee dailywages is " + dailywages + ", Monthly wages is " + monthlyWage);
-		System.out.println("Employee total Salary" + totalWage + " and total Hours is " + totalempHrs);
+		System.out.println("Total Employee Wage for Company " + CompanyName + "  is  " + totalWage);
 	}
 
 }
@@ -53,11 +45,11 @@ class Wages {
 public class EmployeeWages {
 
 	public static void main(String[] args) {
-		int empCheck = 0;
-
 		// declaring constants
 		Wages w = new Wages();
-		w.getRandomNumber();
-		w.empdetails(empCheck);
+		w.empdetails("CMR", 60, 2, 10);
+		w.empdetails("GMR", 120, 8, 30);
+		w.empdetails("SVR_Jew", 30, 8, 30);
+		w.empdetails("Dmart", 20, 2, 10);
 	}
 }
