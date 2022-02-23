9ad72d6b7b5f16e898888662012497471c2bd204
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-02-23 23:22:46 +0530

    Calculating Wages using switch

--- src/com/companywages/EmployeeWages.java
+++ src/com/companywages/EmployeeWages.java
@@ -1,31 +1,42 @@
 package com.companywages;
 
 public class EmployeeWages {
-	public static void empdata(int fulltime, int parttime) {
+	public static int getRandomNumber() {
+		//Generate random number for 0 for absent 1 for present
+		int empCheck=(int)Math.floor(Math.random()*10) % 3;
+		//System.out.println(empCheck);
+		return empCheck;
+
+	}
+	public static void empdata(int empCheck) {
 		final int wagesperhrs = 20;
 		int empHrs = 0;
 		// number for 0 for absent 1 for present
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		empCheck=getRandomNumber();
 
-		// if condition to check employee is working full time, part time or absent
-		if (empCheck == fulltime)
-			empHrs = 16;
-		else if (empCheck == parttime)
-			empHrs = 8;
-		else
-			empHrs = 0;
-		// Daily Wages
+		// Switch to check employee is working full time, part time or absent
+		switch (empCheck) {
+		case 0:
+			empHrs = 0;// Employee absent
+			break;
+		case 1:
+			empHrs = 4;// Employee Parttime present
+			break;
+		default:
+			empHrs = 8;// Employee Fulltime present
+			break;
+		}
+		// Calculating Daily Wages of employee
 		int dailywages = wagesperhrs * empHrs;
 		System.out.println("Employee Wage is " + dailywages);
 
 	}
 
 	public static void main(String[] args) {
-		int parttime = 1, fulltime = 2;
-
+		int empCheck=0;
+		getRandomNumber();
 		// declaring constants
-		empdata(parttime, fulltime);
-
+		empdata(empCheck);
 	}
 
 }
